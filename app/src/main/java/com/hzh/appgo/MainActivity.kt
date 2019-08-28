79ae33730b7938f3fb652df0f9704949b5211078
hao <alccxz@163.com>
2019-08-28 11:55:43 +0800

    太难了

--- app/src/main/java/com/hzh/appgo/MainActivity.kt
+++ app/src/main/java/com/hzh/appgo/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.hzh.appgo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -1,15 +1,19 @@
 object Dep {
     object Version {
         const val kotlin = "1.3.11"
-        const val constraintLayout = "1.1.3"
-        const val appcompat = "28.0.0"
+        const val constraintLayout = "2.0.0-alpha2"
+        const val androidX = "1.0.0"
         // library (test)
         const val junit = "4.12"
+        const val buildGradle = "3.5.0"
     }
 
+    val buildGradle = "com.android.tools.build:gradle:${Version.buildGradle}"
+    val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.kotlin}"
+
     val kotlinStdlibJdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Version.kotlin}"
-    val appcompat = "com.android.support:appcompat-v7:${Version.appcompat}"
-    val constraintLayout = "com.android.support.constraint:constraint-layout:${Version.constraintLayout}"
+    val appcompat = "androidx.appcompat:appcompat:${Version.androidX}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Version.constraintLayout}"
 
 
     //test
